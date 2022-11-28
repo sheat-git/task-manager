23bcaafd8fdfad78d0f4e3e775db34ca5c5465cf
sheat <dev.sheat@gmail.com>
2022-11-28 18:51:38 +0900

    update .env

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 supabase start
-supabase status -o env --override-name api.url=REACT_APP_SUPABASE_URL --override-name auth.anon_key=REACT_APP_SUPABASE_ANON_KEY > .env.local
+supabase status -o env --override-name api.url=REACT_APP_SUPABASE_URL --override-name auth.anon_key=REACT_APP_SUPABASE_ANON_KEY > .env
 supabase functions new deleteUser
 supabase functions serve deleteUser &
 npm start &
